d093f221df6d32c053c17ce2c8cb2acb4a952b58
Xavier Carpentier <xcapetir@gmail.com>
2017-11-13 14:05:30 +0100

    feat(landing): WIP

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,24 @@
-export default () => <div>Welcome to next.js!</div>
+export default () => (
+  <div>
+    <div className="bg-img"></div>
+    <div>Welcome to React Native Gallery!</div>
+    <style jsx>{`
+      .bg-img {
+        background-image: url("/static/images/background.jpeg");
+        background-attachment: scroll;
+        background-position: 50% 50%;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-color: transparent;
+        opacity: .53;
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        content: "";
+        z-index: 0;
+      }
+    `}</style>
+  </div>
+)
