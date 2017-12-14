93f1e38691047e47ed7277abc9fab45bec1c3f99
Xavier Carpentier <xcapetir@gmail.com>
2017-12-14 16:01:39 +0100

    add oauth page

---
+++ pages/oauth.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Oauth = () => (
+  <div className="row">
+      <div className="col-sm-4">
+          <p>oauth page</p>
+      </div>
+  </div>
+)
+
+export default Oauth
