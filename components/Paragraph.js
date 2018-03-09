34deffd7804e296d303c0bede319cd849bcd3adf
Xavier Carpentier <xcapetir@gmail.com>
2018-03-09 19:37:24 +0100

    improve readability

--- components/Paragraph.js
+++ components/Paragraph.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 const Subtitle = styled.p`
-  font-size: 16px;
-  line-height: 25px;
+  font-size: 17px;
+  line-height: 26px;
   @media (max-width: 769px) {
     font-size: 14px;
   }
--- pages/index.js
+++ pages/index.js
@@ -28,26 +28,32 @@ const Home = ({ gifs, type, action }) => (
       <Cell width={12} style={{ maxWidth: 800, width: '100%', margin: 'auto' }}>
         <Paragraph
           style={{
-            backgroundColor: 'rgba(255, 255, 255, 0.8)',
-            padding: 15,
-            borderRadius: 15
+            backgroundColor: 'rgba(255, 255, 255, 0.9)',
+            padding: 30,
+            borderRadius: 15,
+            borderWidth: 1,
+            borderColor: '#BBB8A9',
+            borderStyle: 'solid',
+            boxShadow: '1px 1px 3px grey'
           }}
         >
           <Paragraph>
-            <b>Reactnative.gallery</b> is a website where you can visualize apps and open
-            source components as videos. Created by a react-native developer who realized
-            that a way to visually share applications and simple mobile developments was
-            sorely lacking, in particular for animations, navigation transitions,
-            navigation drawers or simply smooth, fluid applications.
+            <strong>Reactnative.gallery</strong> is a website where you can visualize apps
+            and open source components as videos.<br />
+            <br />Created by a react-native developer who realized that a way to visually
+            share applications and simple mobile developments was sorely lacking, in
+            particular for animations, navigation transitions, navigation drawers or
+            simply smooth, fluid applications.
           </Paragraph>
           <Paragraph>
             It is impossible to show these aspects with simple screenshots. And installing
             the app just to see it is too much hassle.
           </Paragraph>
           <Paragraph>
-            <b>Reactnative.gallery</b> makes it possible to not only visualize apps at a
-            glance using videos, but also to describe the app, categorize it, do a search
-            and above all share it with the rest of the community.
+            <strong>Reactnative.gallery</strong> makes it possible to not only{' '}
+            <strong>visualize apps at a glance</strong> using videos, but also to describe
+            the app, categorize it, do a search and above all{' '}
+            <strong>share it with the rest of the community</strong>.
           </Paragraph>
           <Paragraph>
             GitHub is loaded with react-native repositories containing one or more
@@ -56,8 +62,9 @@ const Home = ({ gifs, type, action }) => (
           </Paragraph>
           <Paragraph>
             For open-source developers, you can login with GitHub and your animated gifs
-            will be automatically recognized and shared, and then can receive feedback
-            from the community (comments and the number of views and likes are displayed).
+            will be <strong>automatically recognized and shared</strong>, and then can
+            receive feedback from the community (comments and the number of views and
+            likes are displayed).
           </Paragraph>
           <Paragraph>
             For those who are searching for a particular component, you can search by
