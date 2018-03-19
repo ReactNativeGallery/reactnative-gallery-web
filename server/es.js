d8063ef17531f8839fe43f38f896caa519d0e0b0
Xavier Carpentier <xcapetir@gmail.com>
2018-03-19 11:44:17 +0100

    fix(deps): es last version

--- server/es.js
+++ server/es.js
@@ -10,21 +10,24 @@ const client = new elasticsearch.Client({
   host: ES_URL
 })
 
-const pingAsync = async () => {
-  await client.ping({ requestTimeout: 1000 })
-}
+const pingAsync = () => client.ping({ requestTimeout: 1000 })
 
 const isIndexExistAsync = index =>
   invariant(index, 'index should be defined') || client.indices.exists({ index })
 
 const isTypeExistAsync = (index, type) =>
   invariant(index, 'index should be defined') ||
-  invariant(index, 'type should be defined') ||
+  invariant(type, 'type should be defined') ||
   client.indices.existsType({ index, type })
 
-const createIndexAsync = index => client.indices.create({ index })
+const createIndexAsync = index =>
+  invariant(index, 'index should be defined') || client.indices.create({ index })
 
-const createTypeAsync = (index, type, body) => client.indices.putMapping({ index, type, body })
+const createTypeAsync = (index, type, body) =>
+  invariant(index, 'index should be defined') ||
+  invariant(type, 'type should be defined') ||
+  invariant(body, 'mapping should be defined') ||
+  client.indices.putMapping({ index, type, body })
 
 const getLocalMappingPath = curry((dirname, index, type) => `${dirname}/mappings/${index}_${type}.json`)
 
@@ -36,14 +39,18 @@ const readFile = filename => fs.readFileSync(filename, { encoding: 'utf8' })
 const getLocalMapping = (index, type) =>
   compose(JSON.parse, readFile, getLocalMappingPath(__dirname))(index, type)
 
-const initIndexTypeAsync = async (index, type, mapping) => {
+const initIndexTypeAsync = async (index, type) => {
   await pingAsync()
   const isIndexExist = await isIndexExistAsync(index)
   if (!isIndexExist) {
     await createIndexAsync(index)
   }
-  const isTypeExist = await isTypeExistAsync(type, mapping)
+  const isTypeExist = await isTypeExistAsync(index, type)
   if (!isTypeExist) {
+    let mapping = {}
+    if (isLocalMappingExist(index, type)) {
+      mapping = getLocalMapping(index, type)
+    }
     createTypeAsync(index, type, mapping)
   }
 }
