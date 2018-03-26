e8e662407921b7a9fdc3da63b8b81230c90a941b
Xavier Carpentier <xcapetir@gmail.com>
2018-03-26 21:34:43 +0200

    chore(*): *

--- pages/login.js
+++ pages/login.js
@@ -6,6 +6,7 @@ import Auth from '../utils/auth'
 const auth = new Auth()
 const Login = () => (
   <Wrapper>
+    <p>...connect with github...</p>
     <button onClick={() => auth.login()}>connect with github</button>
   </Wrapper>
 )
